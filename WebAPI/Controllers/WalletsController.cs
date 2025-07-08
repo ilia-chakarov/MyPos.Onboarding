bd11eb2dc1c0deff6501e4f9f14d321efdafc728
ilia-chakarov <ilia.chakarov@mypos.com>
2025-07-08 17:57:38 +0300

    Added Wallet controller with Create and GetById endpoints

--- WebAPI/Controllers/WalletsController.cs
+++ WebAPI/Controllers/WalletsController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebAPI.DTOs;
+using WebAPI.Entities;
 using WebAPI.UnitOfWork;
 
 namespace WebAPI.Controllers
@@ -13,7 +15,57 @@ namespace WebAPI.Controllers
             _unitOfWork = uow;
         }
 
-        
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateWalletDto dto)
+        {
+            var registrant = await _unitOfWork.RegistrantRepository.GetByIdAsync(dto.RegistrantId);
+            if(registrant == null)
+                return BadRequest($"Registrant with ID {dto.RegistrantId} does not exist.");
+
+            var wallet = new Wallet
+            {
+                DateCreated = DateTime.Now,
+                Status = dto.Status,
+                TarifaCode = dto.TarifaCode,
+                LimitCode = dto.LimitCode,
+                RegistrantId = registrant.Id,
+            };
+
+            await _unitOfWork.WalletRepository.AddAsync(wallet);
+            await _unitOfWork.SaveChangesAsync();
+
+            var walletDto = new WalletDto
+            {
+                Id = wallet.Id,
+                DateCreated = wallet.DateCreated,
+                Status = wallet.Status,
+                TarifaCode = wallet.TarifaCode,
+                LimitCode = wallet.LimitCode,
+                RegistrantId = wallet.RegistrantId
+            };
+            return CreatedAtAction(nameof(GetById), new { id = wallet.Id }, walletDto);
+
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var wallet = await _unitOfWork.WalletRepository.GetByIdAsync(id);
+            if (wallet == null)
+                return NotFound();
+
+            var walletDto = new WalletDto
+            {
+                Id = wallet.Id,
+                DateCreated = wallet.DateCreated,
+                Status = wallet.Status,
+                TarifaCode = wallet.TarifaCode,
+                LimitCode = wallet.LimitCode,
+                RegistrantId = wallet.RegistrantId
+            };
+
+            return Ok(walletDto);
+        }
 
     }
 }
--- WebAPI/DTOs/WalletDto.cs
+++ WebAPI/DTOs/WalletDto.cs
@@ -1,9 +1,9 @@
-﻿using WebAPI.Entities;
-
-namespace WebAPI.DTOs
+﻿namespace WebAPI.DTOs
 {
-    public class CreateWalletDto
+    public class WalletDto
     {
+        public int Id { get; set; }
+        public DateTime DateCreated { get; set; }
         public string Status { get; set; } = null!;
         public string TarifaCode { get; set; } = null!;
         public string LimitCode { get; set; } = null!;
