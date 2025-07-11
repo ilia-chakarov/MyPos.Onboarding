5ee569215a79aee121dee6451027dbabbaa0b5ab
ilia-chakarov <ilia.chakarov@mypos.com>
2025-07-11 11:24:13 +0300

    Save NSwag command help

--- WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cb80819e397469b5d5fb4c1150b1b623111f80e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f3963ffb8a79101a2cf9b3a4eacdc59ae5ef935")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
