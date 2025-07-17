bd8b2f6c75fd3a3e8097aab47122da549f268384
ilia-chakarov <ilia.chakarov@mypos.com>
2025-07-17 10:49:15 +0300

    refactor: move UOW into MyPos.Data

--- WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3306c9eb4683e48f06a3a55df7d75440ae2ddf40")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce616836ee74fc3ea30f339060a5bd91615ff518")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
