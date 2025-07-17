efb53656438f7cad36a7b60aaf0b04cac483142f
ilia-chakarov <ilia.chakarov@mypos.com>
2025-07-17 15:22:55 +0300

    feat: create invalid login test

---
+++ MyPos.WebAPI.xUnitTests/ServicesTests/AuthServiceTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using Moq;
+using WebAPI.DTOs;
+using WebAPI.Entities;
+using WebAPI.Options;
+using WebAPI.Services;
+using WebAPI.UnitOfWork;
+
+namespace MyPos.WebAPI.xUnitTests.ServicesTests
+{
+    public class AuthServiceTests
+    {
+        private readonly Mock<IUnitOfWork> _unitOfWorkMock;
+        private readonly Mock<IPasswordHasher<UserEntity>> _passwordHasherMock;
+        private IOptions<JwtSettings> _jwtOptions;
+
+        private readonly AuthService _authService;
+        public AuthServiceTests()
+        {
+            _unitOfWorkMock = new Mock<IUnitOfWork>();
+            _passwordHasherMock = new Mock<IPasswordHasher<UserEntity>>();
+            _jwtOptions = Options.Create(new JwtSettings
+                {
+                SecretKey = "test-secret-key",
+                Issuer = "test-issuer",
+                Audience = "test-audience",
+                ExpiryMinutes = 60
+                }
+            );
+
+            _authService = new AuthService( _unitOfWorkMock.Object, _passwordHasherMock.Object, _jwtOptions );
+        }
+
+        [Fact]
+        public void Login_WithInvalidCredentials_ReturnsNull()
+        {
+            var loginDto = new LoginDto { Username = "wrong", Password = "invalid_pass" };
+
+            var res = _authService.Login( loginDto );
+
+            Assert.NotNull( res );
+        }
+    }
+}
