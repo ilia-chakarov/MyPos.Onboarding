abbf8f9281521a9391e749e3ac280f0a1e4ccad1
ilia-chakarov <ilia.chakarov@mypos.com>
2025-07-31 10:50:29 +0300

    feat: add counting and registrant names to user dto

---
+++ MyPos.Services/DTOs/CountDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPos.Services.DTOs
+{
+    public class CountDto
+    {
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public int TotalCount { get; set; }
+    }
+}
---
+++ MyPos.Services/DTOs/UserCountedDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPos.Services.DTOs
+{
+    public class UserCountedDto
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = null!;
+        public int RegistrantId { get; set; }
+        public string RegistrantName { get; set; } = null!;
+        public CountDto CountDto { get; set; } = null!;
+    }
+}
